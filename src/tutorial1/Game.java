d80905585e5b7a12da38fc5abf3c0cf7fe3294dc
Gabriel Ortega <A01176807@itesm.mx>
2019-02-27 17:03:22 -0600

    Pausa 1.2

--- src/tutorial1/Game.java
+++ src/tutorial1/Game.java
@@ -6,6 +6,7 @@
 package tutorial1;
 //GABO
 //Natalia
+
 import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferStrategy;
@@ -14,8 +15,8 @@ import java.awt.image.BufferStrategy;
  *
  * @author HOME
  */
-public class Game implements Runnable{
-    
+public class Game implements Runnable {
+
     private BufferStrategy bs;
     private Graphics g;
     private Display display;
@@ -28,8 +29,8 @@ public class Game implements Runnable{
     private Ball ball;              //Variable de tipo Ball
     private KeyManager keyManager;
     private boolean start;          //Booleana que controlara el inicio
-    
-    public Game(String title, int width, int height){
+
+    public Game(String title, int width, int height) {
         this.title = title;
         this.width = width;
         this.height = height;
@@ -45,23 +46,23 @@ public class Game implements Runnable{
     public void setStart(boolean start) {
         this.start = start;
     }
-    
-    public int getWidth(){
+
+    public int getWidth() {
         return width;
     }
 
     public int getHeight() {
         return height;
     }
-    
-    private void init(){
+
+    private void init() {
         display = new Display(title, getWidth(), getHeight());
         Assets.init();
         player = new Player(330, getHeight() - 100, 1, 160, 80, this);
         ball = new Ball(385, getHeight() - 145, 1, 50, 50, this);
         display.getJframe().addKeyListener(keyManager);
     }
-    
+
     @Override
     public void run() {
         init();
@@ -75,61 +76,62 @@ public class Game implements Runnable{
         long now;
         // initializing last time to the computer time in nanosecs
         long lastTime = System.nanoTime();
-        while (running){
+        while (running) {
             // setting the time now to the actual time
             now = System.nanoTime();
             // acumulating to delta the difference between times in timeTick units
             delta += (now - lastTime) / timeTick;
             //updating the last time
             lastTime = now;
-            
+
             // if delta is positive we tick the game
-            if (delta >= 1){
+            if (delta >= 1) {
                 tick();
                 render();
-                delta --;
+                delta--;
             }
         }
-        stop();    
+        stop();
     }
 
     public KeyManager getKeyManager() {
         return keyManager;
     }
-    
-    private void tick(){
+
+    private void tick() {
         keyManager.tick();
 
         player.tick();
         ball.tick();
         //Si se preciona space
-        if (getKeyManager().space){
+        if (getKeyManager().space) {
             //Se colocara start en true
             setStart(true);
         }
-        //Si la pelota intersecta con el player
-        if (player.intersecta(ball)){
-            //Si la bola se mueve en la direccion 3 (Abajo a la derecha)
-            if (ball.getDirection() == 3){
-                //Se cambia la direccion a 1 (Arriba a la derecha)
-                ball.setDirection(1);
-            }
-            //En el caso contrario
-            else{
-                //Se cambia la direcaccion a 2 (Arriba a la izquierda)
-                ball.setDirection(2);
-            }
+        //Si la pelota intersecta con el player en la mitad derecha
+        if (player.intersecta(ball)) {
+            ball.setDirection(2);
+        }
+        //Si la pelota intersecta con el player en la mitad derecha
+        else if (player.intersecta2(ball)) {
+            ball.setDirection(1);
+        }
+        //Si la pelota sale de la pantalla por abajo
+        if(ball.getY() >= getHeight()){
+            setStart(false);
+            ball.setX(385);
+            ball.setY(getHeight()-145);
+            player.setX(330);
+            player.setY(getHeight()-100);
         }
     }
-    
-    private void render(){
+
+    private void render() {
         bs = display.getCanvas().getBufferStrategy();
-        
-        if(bs == null){
+
+        if (bs == null) {
             display.getCanvas().createBufferStrategy(3);
-        }
-        else
-        {
+        } else {
             g = bs.getDrawGraphics();
             g.drawImage(Assets.background, 0, 0, width, height, null);
             player.render(g);
@@ -138,21 +140,21 @@ public class Game implements Runnable{
             g.dispose();
         }
     }
-    
-    public synchronized void start(){
-        if(!running){
+
+    public synchronized void start() {
+        if (!running) {
             running = true;
             thread = new Thread(this);
             thread.start();
         }
     }
-    
-    public synchronized void stop(){
-        if(running){
+
+    public synchronized void stop() {
+        if (running) {
             running = false;
-            try{
+            try {
                 thread.join();
-            }catch (InterruptedException ie){
+            } catch (InterruptedException ie) {
                 ie.printStackTrace();
             }
         }
